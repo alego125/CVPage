7070715877140d4e1b17cbf2d3666dff2f2ded31
Ale Gomez <alejandro.gomez969@gmail.com>
2022-07-21 12:22:16 -0300

    Agregado inicio sesion automatico

--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacionService } from '../servicios/autenticacion.service';
 
 @Component({
   selector: 'app-initial-page',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private autenticacionService:AutenticacionService,
+    private ruta:Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  entrarSinUsuario(){
+    let userLogin = {    
+      "nombreUsuario":"user",
+      "password":"user"     
+  }
+    this.autenticacionService.iniciarSesion(userLogin).subscribe(
+      data=>{
+        //Mostramos por consola los datos devueltos por el servicio
+        //Si la autenticacion es correcta redirigimos al usuario a la ruta de portfolio para mostrar el contenido
+        setTimeout(()=>{
+          this.ruta.navigate(['/portfolio']);
+        },4000)
+      }
+    
+    ) 
   }
 
 }
