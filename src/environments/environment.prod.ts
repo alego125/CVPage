d071f89a26dbb38a77e520085c05b2a152d7070d
Ale Gomez <alejandro.gomez969@gmail.com>
2022-07-22 21:46:28 -0300

    agregado git ignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBIx9JI4ofUB7I7cTczfsjTRbiSe8RNA5c",
+    authDomain: "fotos-cd2ed.firebaseapp.com",
+    projectId: "fotos-cd2ed",
+    storageBucket: "fotos-cd2ed.appspot.com",
+    messagingSenderId: "339909514913",
+    appId: "1:339909514913:web:0533ca9a02c10e27c742e0",
+    measurementId: "G-Z2X1KCSL76"
+  },
+  urlServer:"https://agile-garden-14828.herokuapp.com/" 
+  // urlServer:"https://localhost:8080"
 };
